b1e8817068202556898e857f227d2d0078ee96d4
Tong Yin <tongyin002@gmail.com>
2020-02-27 11:51:27 -0800

    tyin~editor processOnChange done

--- src/controller.js
+++ src/controller.js
@@ -1,19 +1,29 @@
+import Editor from "./editor";
+import BroadCast from "./broadcast";
+import CRDT from "./crdt";
+import Peer from "peerjs";
+
 class Controller {
-  constructor(editor, broadcast, crdt, peer) {
+  constructor(host, targetPeerId) {
     this.siteId = null;
-    this.editor = editor;
-    this.broadcast = broadcast;
-    this.crdt = crdt;
+    this.editor = new Editor(this);
+    this.broadcast = new BroadCast();
+    this.crdt = new CRDT();
+    this.peer = new Peer();
   }
 
   // create new editor
   createEditor() {}
 
   // insert to editor
-  localInsert() {}
+  localInsert(text, from) {
+    console.log(text, from);
+  }
 
   // delete from editor
-  localDelete() {}
+  localDelete(text, from, to) {
+    console.log(text, from, to);
+  }
 
   broadcast(char) {}
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import Controller from "./controller";
-import Editor from "./editor";
-import CRDT from "./crdt";
-import BroadCast from "./broadcast";
-import Peer from "peerjs";
 
 let controller = new Controller(
-  new Editor(),
-  new BroadCast(),
-  new CRDT(),
-  new Peer()
+  location.origin,
+  location.search.slice(1) || "0"
 );
